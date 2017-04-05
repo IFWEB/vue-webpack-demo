fc47c500c6eb567a22159e29e376244c02729ae3
wangxjsteven <525_wxj@163.com>
2017-04-05 15:04:00 +0800

    format bueaty!

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,19 @@
 var webpack = require('webpack');
 
-module.exports = { 
-  entry: { app: './src/app.js', vendor: [ 'vue' ] },
-  output:
-   { path: '/Users/liyicheng/workspace/vuejs/demo/dist',
-     filename: '[name].[chunkhash].js',
-     publicPath: 'static/',
-     chunkFilename: '[id].[chunkhash].js' },
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor'
-    })
-  ]
+module.exports = {
+    entry: {
+        app: './src/app.js',
+        vendor: ['vue']
+    },
+    output: {
+        path: '/Users/liyicheng/workspace/vuejs/demo/dist',
+        filename: '[name].[chunkhash].js',
+        publicPath: 'static/',
+        chunkFilename: '[id].[chunkhash].js'
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor'
+        })
+    ]
 }
